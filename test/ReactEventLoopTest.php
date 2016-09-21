bfe419595297f707f5335898c7b9a2dbe15fde39
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-09-21 07:51:57 +0200

    Use the factory to get the loop

--- test/ReactEventLoopTest.php
+++ test/ReactEventLoopTest.php
@@ -3,13 +3,12 @@
 namespace WyriHaximus\React\Tests\AsyncInteropLoop;
 
 use Interop\Async\Loop\Test;
-use React\EventLoop\StreamSelectLoop;
 use WyriHaximus\React\AsyncInteropLoop\ReactDriverFactory;
 
 class ReactEventLoopTest extends Test
 {
     public function getFactory()
     {
-        return ReactDriverFactory::createFactoryFromLoop(StreamSelectLoop::class);
+        return ReactDriverFactory::createFactory();
     }
 }
