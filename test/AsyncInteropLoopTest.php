90176318ef123b7982d89c9aeda83b9347f5fbd6
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-10-12 17:11:29 +0200

    Use the factory

--- test/AsyncInteropLoopTest.php
+++ test/AsyncInteropLoopTest.php
@@ -3,8 +3,6 @@
 namespace WyriHaximus\React\Tests\AsyncInteropLoop;
 
 use Interop\Async\Loop;
-use Interop\Async\Loop\Test;
-use React\EventLoop\StreamSelectLoop;
 use React\Tests\EventLoop\AbstractLoopTest;
 use WyriHaximus\React\AsyncInteropLoop\AsyncInteropLoop;
 use WyriHaximus\React\AsyncInteropLoop\ReactDriverFactory;
@@ -13,7 +11,7 @@ class AsyncInteropLoopTest extends AbstractLoopTest
 {
     public function createLoop()
     {
-        Loop::setFactory(ReactDriverFactory::createFactoryFromLoop(StreamSelectLoop::class));
+        Loop::setFactory(ReactDriverFactory::createFactory());
         return new AsyncInteropLoop();
     }
 }
